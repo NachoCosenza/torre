2b8be5a92580b9e1843f9db660a0b88f7b13829a
NachoCosenza <cosenza.lores@hotmail.com>
2017-07-11 18:29:14 -0300

    cosenza
    tower bolck

---
+++ torre/Assets/generador.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class generador : MonoBehaviour {
+	private Rigidbody rb;
+	private int fuerza = 10;
+	public GameObject caja;
+	public Transform salida;
+	void Start ()
+	{
+		rb = GetComponent<Rigidbody> ();
+
+	}
+	
+
+	void Update ()
+	{
+		transform.Translate (Vector3.left * Time.deltaTime * fuerza);
+
+		if (Input.GetKeyDown(KeyCode.Space)) 
+		{
+			Instantiate (caja, salida.position,salida.rotation);
+			transform.Translate(Vector3.up * Time.deltaTime * 120);
+			
+		}
+	}
+
+	void OnTriggerEnter(Collider other) 
+	{
+		print ("si");
+		fuerza = fuerza * -1;
+	
+	}
+
+
+}
